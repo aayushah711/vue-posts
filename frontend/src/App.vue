eb61d918ac1f50a0612ab6d84ddcd6dfbfd50f3e
Aayushi <aayu.shah711@gmail.com>
2020-12-10 10:25:30 +0530

    Added video tag & styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,43 @@
         components: { PostItem },
         data() {
             return {
-                data: { csv: null },
+                data: {
+                    // csv: null,
+                    csv: [
+                        {
+                            url:
+                                "https://cdn.sharechat.com/1a40b23d_1606974965867_sc.mp4",
+                            userId: 59096277,
+                            postId: 5906298332,
+                            tagGenre: "Humour and Fun",
+                            type: "video",
+                            language: "Marathi",
+                            shares: 10,
+                            likes: 35,
+                            favs: 13,
+                            comment_created: 0,
+                            views: 5429,
+                            eByV: 0.010683367,
+                            tagName: "tagName",
+                        },
+                        {
+                            url:
+                                "https://cdn.sharechat.com/25fa0daa_1606900878523_sc.mp4",
+                            userId: 426114987,
+                            postId: 9564235332,
+                            tagGenre: "Politics",
+                            type: "video",
+                            language: "Punjabi",
+                            shares: 70,
+                            likes: 695,
+                            favs: 100,
+                            comment_created: 19,
+                            views: 15459,
+                            eByV: 0.057183518,
+                            tagName: "tagName",
+                        },
+                    ],
+                },
                 loading: false,
             };
         },
@@ -78,6 +114,11 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+        padding: 2vh;
+        min-height: 98vh;
+        background: #fafafa;
+    }
+    body {
+        margin: 0;
     }
 </style>
--- frontend/src/components/PostItem.vue
+++ frontend/src/components/PostItem.vue
@@ -1,9 +1,48 @@
 <template>
-    <div>{{ post.postId }}</div>
+    <section class="post-container">
+        <div class="player-container">
+            <video controls>
+                <source :src="post.url" />
+            </video>
+            <!-- <vue-core-video-player :src="post.url"></vue-core-video-player> -->
+
+            <!-- <video width="100" controls>
+                <source :src="post.url" type="video/mp4" />
+
+                Sorry, your browser doesn't support embedded videos.
+            </video> -->
+        </div>
+        <p>{{ post.postId }}</p>
+    </section>
 </template>
 
 <script>
+    // import VueCoreVideoPlayer from "vue-core-video-player";
+    // import videoPlayer from "vuetify-media-player/src/components/video-player.vue";
+
     export default {
         props: ["post"],
     };
 </script>
+
+<style scoped>
+    .post-container {
+        /* height: 600px; */
+        width: 400px;
+        margin: 2vh auto;
+        padding: 2vh;
+
+        background-color: white;
+    }
+
+    .player-container {
+        /* height: 600px; */
+        /* width: 400px; */
+    }
+    video {
+        /* override other styles to make responsive */
+        width: 80% !important;
+        height: auto !important;
+        padding: 0 2vh;
+    }
+</style>
